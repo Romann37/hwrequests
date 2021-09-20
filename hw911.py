7cb5f391ae2ffeede1db9f6ea32c70b5b83c1d9c
Romann37 <mihalkovo62@gmail.com>
2021-09-20 16:12:41 +0300

    исправил задачу 2

--- hw911.py
+++ hw911.py
@@ -1,33 +1,20 @@
-from pprint import pprint
+
 import requests
 
-def charahter(url1, url2, url3):
-    print("интелект Капитан Америка")
-    response = requests.get(url1, timeout=5)
-    text1 = response.json()
-    int_cap = text1.get('intelligence')
-    print(int_cap)
-    #pprint(text1)
-    print("интеллект Халк")
-    response = requests.get(url2, timeout=5)
-    text1 = response.json()
-    int_hulk = text1.get('intelligence')
-    print(int_hulk)
-    #pprint(text1)
-    print("интеллект Танос")
-    response = requests.get(url3, timeout=5)
-    text1 = response.json()
-    int_tanos = text1.get('intelligence')
-    print(int_tanos)
-    #pprint(text1)
-    listprint = []
-    listprint.append(int_cap)
-    listprint.append(int_hulk)
-    listprint.append(int_tanos)
-    print(f"самый умный супергерой: {max(listprint)}")
-url1 = "https://superheroapi.com/api/2619421814940190/149/powerstats/get"
-url2 = "https://superheroapi.com/api/2619421814940190/332/powerstats/get"
-url3 = "https://superheroapi.com/api/2619421814940190/665/powerstats/get"
-heroes = charahter(url1, url2, url3)
-    
+names_list = ['Captain America', 'Hulk', 'Thanos']
+url = "https://superheroapi.com/api/2619421814940190/search/"
+def heroes_int(names_list, url):
+    url_list = []
+    heroes_dict = {}
+    for i_id in names_list:
+        url_list.append(url + i_id)
+    for hero in url_list:
+        response = requests.get(hero)
+        names = response.json()['results'][0]['name']
+        intelligence = int(response.json()['results'][0]['powerstats']['intelligence'])
+        heroes_dict[names] = intelligence
+    return print(f'Самым умным супергероем оказался - {max(heroes_dict)}')
+
+
+heroes_int(names_list, url)    
 
